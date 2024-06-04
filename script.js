9b03b51898c0ae33a607eeb8ed2da828a8505b0a
Henrique <venoshk.henrique19@gmail.com>
2024-06-04 12:10:55 -0300

    Formatação de número de telefone, formatação e validação  do cep, e criação de codigo tanto para o admin quanto para o usuario

--- script.js
+++ script.js
@@ -1,162 +1,89 @@
-const cartClose = document.querySelector("#cart")
-const cartOpen = document.querySelector("#open-cart");
-const CartItem = document.querySelector(".add-cart-item");
-const menu     = document.querySelector("#menu");
-const list     = document.querySelector("#cart-item");
+//fechar/abrir/adicionar
+const cartClose    = document.querySelector("#cart");
+const closeCartBtn = document.querySelector('#close-cart');
+const cartOpen     = document.querySelector("#open-cart");
+const CartItem     = document.querySelector(".add-cart-item");
+//Lista
+const menu       = document.querySelector("#menu");
+const list       = document.querySelector("#cart-item");
 const totalPrice = document.querySelector('#total-value')
-const items      = document.querySelector('#count')
-const closeCart  = document.querySelector('#close-cart')
-const form               = document.querySelector(".form");
-const inputName          = document.querySelector("#alert-warn-name");
-const alertSpanNome      = document.querySelector(".alert-warn-span");
-const inputCep           = document.querySelector('#alert-warn-cep');
-const alertSpanCep       = document.querySelector('.alert-span-cep')
-const btnEnv             = document.querySelector("#env-btn");
-const alertCart          = document.querySelector("#alert-card");
-const alertCep           = document.querySelector("#alert-warn-cep")
-const endereço           = document.querySelector("#endereço");
-const dataOpen           = document.querySelector("#data");
-const cart = []
+const items      = document.querySelector('#count');
+//Form
+const form          = document.querySelector(".form");
+const nameClient    = document.querySelector("#name");
+const nameWarn      = document.querySelector("#alert-name-span");
+const homeClient    = document.querySelector("#home");
+const homeWarn      = document.querySelector("#alert-home-span");
+const cep           = document.querySelector('#cep');
+const cepWarn       = document.querySelector('#alert-cep-span');
+const address       = document.querySelector("#address");
+const addressWarn   = document.querySelector("#alert-address-span");
+const message       = document.querySelector("#message");
+const btnEnv        = document.querySelector("#env-btn");
+const alertCart     = document.querySelector("#alert-card");
+const phoneNumber   = document.querySelector("#phone-number");
 
-const checkRestOpen = () =>{
+
+//Open/Close restaurente 
+const dateOpen = document.querySelector("#data");
+let cart = [];
+
+const checkRestOpen = () => {
     const date = new Date;
-    const hours = date.getHours();
+    const hours = 20//date.getHours();
     return hours >= 18 && hours <= 22
-    
 }
 const isOpen = checkRestOpen();
 
-if(isOpen){
-    dataOpen.classList.remove('bg-red-500');
-    dataOpen.classList.add('bg-green-700');
-}else{
-    dataOpen.classList.add('bg-red-500');
-    dataOpen.classList.remove('bg-green-700');
+if (isOpen) {
+    dateOpen.classList.remove('bg-red-500');
+    dateOpen.classList.add('bg-green-700');
+} else {
+    dateOpen.classList.add('bg-red-500');
+    dateOpen.classList.remove('bg-green-700');
 }
 
-//Abrir Carrinho
-cartOpen.addEventListener("click", () =>{
-    upDateCart()
-    cartClose.style.display = 'flex'
-});
-     
-//Fechar carrinho
-cartClose.addEventListener("click", (e) =>{
-    if(e.target === cartClose){
-        cartClose.style.display = 'none'
-    }
-})
-
-closeCart.addEventListener('click', () => {
-    cartClose.style.display = 'none'
-})
 
 
-//Eventos
-
-menu.addEventListener('click', (e) =>{
-    let parentButton = e.target.closest(".add-cart-item");
-    const name =  parentButton.getAttribute("data-name")
-    const price = parseFloat(parentButton.getAttribute("data-price"))
-
-    cartAdd(name, price)
-
-});
-
-//Buscar CEP
-inputCep.addEventListener("input", () => {
-    const cepValue = inputCep.value.replace(/\D/g, '');
-
-
-    fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
-    .then(res => res.json()
-    .then(data => {
-        if(data.erro){
-            alertCep.innerHTML = "CEP INVALIDO"
-        }else{
-            endereço.textContent = `${data.cep}, ${data.logradouro}, ${data.bairro}, ${data.uf}`
-        }
-    })
-)
-})
-
-inputName.addEventListener("input", (e) => {
-    const inputValue = e.target.value;
-    if(inputValue !== ""){
-        inputName.classList.remove('border-red-600')
-        alertSpanNome.classList.add('hidden')
-    }
-});
-
-inputCep.addEventListener("input", (e) =>{
-    const inputValue = e.target.value;
-
-    if(inputValue.value !== ""){
-        inputCep.classList.remove('border-red-600')
-        alertSpanCep.classList.add('hidden')
-    }
-})
-
-//Finalizar pedido
-btnEnv.addEventListener('click', () =>{
-
-    const isOpen = checkRestOpen();
-
-    if(!isOpen){
-       Toastify({
-        text: "Ops Restaurante está fechado!",
-        duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "#ef4444",
-        },
-       }).showToast();
-        return;
-    }
-
-    if(cart.length === 0){
-        alertCart.classList.remove('hidden')
-        return;
-    }
-
-    if(inputName.value === ""){
-        inputName.classList.add('border-red-600')
-        inputName.classList.remove('hidden')
-        alertSpanNome.classList.remove('hidden')
-        inputCep.classList.add('border-red-600')
-        alertSpanCep.classList.remove('hidden')
-        return;
-    }
-
-    //Enviar pedido 
-    const cartItems = cart.map((item) => {
-        return(
-            `${item.name} Quantidade: (${item.quantify}), R$${item.price} | `
-        )
-
-    }).join("")
-
-    const menssage = encodeURIComponent(cartItems);
-
-    const phone = "61982410552"
-
-    open(`https://wa.me/${phone}?text=${menssage} Nome do cliente: R${inputName.value} Endereço: ${endereço.value}`, "_black" );
-
-    cart = []
-    upDateCart();
-})
 
 //funções 
+const confirmRequestUser = (code, total) =>{
+    const cartItems = cart.map((item) => {
+        let price = parseFloat(item.price);
+        return (
+            `${item.name} Quantidade: *(${item.quantify})*, - *R$${price.toFixed(2)}*,  | `
+        )
+    }).join("")
+    
+    
+    const menssage = encodeURIComponent(cartItems);
 
-const cartAdd = (name, price) =>{
+    const phone = "61982410552";
+
+    const url = `https://wa.me/${phone}?text=${menssage}, valor a ser pago:*R$${total.toFixed(2)}*  Para confirmar seu pedido envie seu codigo: ${code} `;
+
+    // Abrir link automaticamente
+    window.location.href = url;
+}
+
+//Adicionar ao carrinho
+const cartAdd = (name, price) => {
     const existingItem = cart.find(item => item.name === name);
 
-    if(existingItem){
+    if (existingItem) {
         existingItem.quantify += 1;
-    }else{
+        Toastify({
+            text: "Adicionado no carrinho",
+            duration: 3000,
+            close: false,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#2da52d",
+            },
+        }).showToast();
+    } else {
         cart.push({
             name,
             price,
@@ -170,24 +97,25 @@ const cartAdd = (name, price) =>{
             position: "right", // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-              background: "#2da52d",
+                background: "#2da52d",
             },
-           }).showToast();
+        }).showToast();
     }
 
     upDateCart()
 }
 
-const upDateCart = () =>{
+//atualizar carrinho
+const upDateCart = () => {
     list.innerHTML = "";
     let total = 0
     totalPrice.innerHTML = total
     cart.forEach(item => {
-       const cartItemCreate = document.createElement('div');
-       const totalPrice = item.price * item.quantify;
-       total += totalPrice; 
-       
-       cartItemCreate.innerHTML = `
+        const cartItemCreate = document.createElement('div');
+        const totalPrice = item.price * item.quantify;
+        total += totalPrice;
+
+        cartItemCreate.innerHTML = `
            <div class="border-b-2">
                 <div class="flex gap-8 justify-between items-center">
                     <div class="flex flex-col gap-2">
@@ -202,7 +130,7 @@ const upDateCart = () =>{
                 </div>
            </div>
        `
-       list.appendChild(cartItemCreate);
+        list.appendChild(cartItemCreate);
     })
 
     totalPrice.textContent = total.toLocaleString('pt-BR', {
@@ -211,27 +139,16 @@ const upDateCart = () =>{
     })
 
     items.textContent = cart.length
-}
+};
 
-
-
-//Remoção de item da lista
-
-list.addEventListener("click", (e) => {
-    if(e.target.classList.contains("remove-btn")){
-        const name = e.target.getAttribute("data-name");
-
-        removeItemCart(name)
-    }
-})
-
+//remover items
 const removeItemCart = (name) => {
     const index = cart.findIndex(item => item.name === name);
 
-    if(index !== -1){
+    if (index !== -1) {
         const item = cart[index];
 
-        if(item.quantify > 1){
+        if (item.quantify > 1) {
             item.quantify -= 1
             upDateCart();
             return;
@@ -245,19 +162,266 @@ const removeItemCart = (name) => {
             position: "left", // `left`, `center` or `right`
             stopOnFocus: true, // Prevents dismissing of toast on hover
             style: {
-              background: "#2da52d",
+                background: "#2da52d",
             },
-           }).showToast();
+        }).showToast();
 
         cart.splice(index, 1);
         upDateCart();
     }
 };
 
+//limpar inputs
+const inputsRemoveValues = () => {
+    nameClient.value = "";
+    cep.value        = "";
+    message.value    = "";
+    homeClient.value = "";
+    address.value    = "";
+};
+///Criar codigo
+const generateConfirmationCode = () => {
+    return Math.floor(Math.random() * 1000000)
+};
+
+//validar number-phone
+const formatPhoneNumber = (e) =>{
+    const input = e.target;
+            let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+            const maxLength = 11;
+
+            // Limita o valor ao comprimento máximo de 12 dígitos
+            if (value.length > maxLength) {
+                value = value.slice(0, maxLength);
+            }
+
+            // Adiciona a formatação (61) e o hífen após os primeiros 5 números
+            if (value.length > 2) {
+                value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
+            }
+            if (value.length > 8) {
+                value = `${value.slice(0, 10)}-${value.slice(10)}`;
+            }
+
+            input.value = value;
+};
+
+//validar Cep
+const formatCep = (e) =>{
+    const input = e.target;
+            let value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+            const maxLength = 8;
+
+            // Limita o valor ao comprimento máximo de 12 dígitos
+            if (value.length > maxLength) {
+                value = value.slice(0, maxLength);
+            }
+
+            if (value.length > 8) {
+                value = `${value.slice(0, 10)}-${value.slice(10)}`;
+            }
+
+            input.value = value;
+};
+
+//Eventos
+
+//Abrir Carrinho
+cartOpen.addEventListener("click", () => {
+    upDateCart()
+    cartClose.style.display = 'flex'
+});
+
+//Fechar carrinho
+cartClose.addEventListener("click", (e) => {
+    if (e.target === cartClose) {
+        cartClose.style.display = 'none'
+    }
+});
+
+closeCartBtn.addEventListener('click', () => {
+    cartClose.style.display = 'none'
+});
+
+//Remoção de item da lista
+list.addEventListener("click", (e) => {
+    if (e.target.classList.contains("remove-btn")) {
+        const name = e.target.getAttribute("data-name");
+        removeItemCart(name)
+    }
+});
+
+//formatação de number
+phoneNumber.addEventListener("input", formatPhoneNumber);
+
+menu.addEventListener('click', (e) => {
+    let parentButton = e.target.closest(".add-cart-item");
+    const name = parentButton.getAttribute("data-name")
+    const price = parseFloat(parentButton.getAttribute("data-price"))
+
+    cartAdd(name, price)
+
+});
+
+//Buscar CEP
+cep.addEventListener("input", async () => {
+    const cepValue = cep.value.replace(/\D/g, '');
+
+    await fetch(`https://viacep.com.br/ws/${cepValue}/json/`)
+        .then(res => res.json()
+            .then(data => {
+                if (data.erro) {
+                    alertCep.innerHTML = "CEP INVALIDO"
+                } else {
+                    address.value = `${data.cep}, ${data.logradouro}, ${data.bairro}, ${data.uf}`
+                }
+            })
+        )
+});
+
+//validação de cep
+cep.addEventListener("input", formatCep);
+
+//Verificação de cep
+cep.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+
+    if (inputValue !== "") {
+        cep.classList.remove('border-red-600')
+        cepWarn.classList.add('hidden')
+    }
+});
+
+//Verificação de nome
+nameClient.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+
+    if (inputValue !== "") {
+        nameClient.classList.remove('border-red-600')
+        nameWarn.classList.add('hidden')
+    }
+});
 
 
 
+//Verificação de endereço
+address.addEventListener("input", (e) =>{
+    const inputValue = e.target.value;
 
+    if(inputValue !== ""){
+        address.classList.remove("border-red-600");
+        addressWarn.classList.add("hidden")
+    }
+});
 
+//Verificação de numero da casa ou apt
+homeClient.addEventListener("input", (e) =>{
+    const inputValue = e.target.value;
 
+    if(inputValue !== ""){
+        homeClient.classList.remove("border-red-600")
+        alertCart.classList.add("hidden")
+    }
+});
+
+//Finalizar pedido
+btnEnv.addEventListener('click', async (e) => {
+    console.log(e.target)
+    e.preventDefault();
+    const isOpen = checkRestOpen();
+    const code   = generateConfirmationCode();
+
+    if (!isOpen) {
+        Toastify({
+            text: "Ops Restaurante está fechado!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    };
+
+    if (cart.length === 0) {
+        alertCart.classList.remove('hidden')
+        return;
+    };
+
+    if (nameClient.value === "" || cep.value === "" || address.value === "" || homeClient.value === "") {
+        nameClient.classList.add('border-red-600');
+        nameClient.classList.remove('hidden');
+        nameWarn.classList.remove('hidden');
+        cep.classList.add('border-red-600');
+        cepWarn.classList.remove('hidden');
+        address.classList.add('border-red-600');
+        addressWarn.classList.remove('hidden');
+        homeClient.classList.add("border-red-600");
+        homeWarn.classList.remove("hidden");
+        return;
+    };
+    
+    const totalPagar = cart.reduce((total, item) => total + (item.price * item.quantify), 0);
+
+    //envio para o admin
+    await fetch('http://localhost:3000/enviar-pedido-whatsapp', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: nameClient.value,
+            address: address.value,
+            number: phoneNumber.value,
+            cart: cart,
+            message: message.value,
+            home: homeClient.value,
+            total: totalPagar,
+            code: code
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            let total = parseFloat(totalPagar);
+            confirmRequestUser(code, total)
+          
+            Toastify({
+                text: "Pedido enviado com sucesso!",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "#2da52d",
+                },
+            }).showToast();
+    
+            cart = [];
+            inputsRemoveValues();
+            upDateCart();
+            
+        } else {
+            // Se houver algum erro na resposta do servidor
+            Toastify({
+                text: "Pedido não enviado, tente novamente!",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "red",
+                },
+            }).showToast();
+        }
+    })
+    .catch(_error => {
+        // Se houver um erro na requisição
+        console.error('Erro ao enviar pedido:', _error);
+    });
+});
 
